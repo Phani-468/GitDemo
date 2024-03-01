44f5e8dc5ddabb7044ac5ed2c343fc8aea206a2a
Phani-468 <phanikumar.g68@gmail.com>
2024-03-02 01:52:01 +0530

    2nd person changes

--- src/test/java/Academy/BrowserTest.java
+++ src/test/java/Academy/BrowserTest.java
@@ -16,16 +16,16 @@ public class BrowserTest {
 	@Test
 	public void getData()
 	{
-		System.out.println("Hello Guys");
+		System.out.println("Hello Guys Phani");
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		System.out.println("Hello Guys");
+		System.out.println("Hello Guys phani");
 		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
 		String text =driver.findElement(By.tagName("h1")).getText();
 		System.out.println(text);
 		Assert.assertTrue(text.equalsIgnoreCase("http://localhost:8080/webapp/"));
 		driver.close();
-		System.out.println("Hello Guys");
+		System.out.println("Hello Guys phani");
 	
 		
 		
