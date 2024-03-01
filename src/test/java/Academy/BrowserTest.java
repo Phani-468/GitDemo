cb8e4dc1bbe8c434886381d49613b17e3375f0ed
Phani-468 <phanikumar.g68@gmail.com>
2024-03-02 01:25:28 +0530

    first commit

---
+++ src/test/java/Academy/BrowserTest.java
@@ -0,0 +1,32 @@
+package Academy;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+public class BrowserTest {
+
+	
+	@Test
+	public void getData()
+	{
+		System.out.println("Hello Guys");
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://rahylshettyacademy.azurewebsites.net/webapp/");
+		String text =driver.findElement(By.tagName("h1")).getText();
+		System.out.println(text);
+		Assert.assertTrue(text.equalsIgnoreCase("http://localhost:8080/webapp/"));
+		driver.close();
+	
+		
+		
+	}
+}
